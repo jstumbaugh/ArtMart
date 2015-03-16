e45b9b3ed596d0f96a1a916ce38ff9c40e08700c
Jaemen Moore <jaemenm@gmail.com>
2015-03-16 00:34:28 -0500

    Database creation script migrationy thing finished

--- laravel/database/migrations/2015_03_12_210109_create_database.php
+++ laravel/database/migrations/2015_03_12_210109_create_database.php
@@ -26,14 +26,64 @@ class CreateDatabase extends Migration {
 			$table->timestamps();
 		});
 		
+		Schema::create('wishlists', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('user_id')->unsigned()->default(0);
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->string('entry');
+		});
+		
+		Schema::create('searchhistory', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('user_id')->unsigned()->default(0);
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->string('query');
+			$table->integer('numresults');
+		});
+		
+		Schema::create('licenses', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->string('url');
+		});
+		
 		Schema::create('artworks', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->integer('user_id')->unsigned()->default(0);
 			$table->foreign('user_id')->references('id')->on('users');
+			$table->integer('license_id')->unsigned()->default(0);
+			$table->foreign('license_id')->references('id')->on('licenses');
 			$table->string('title');
 			$table->string('description');
 			$table->decimal('price', 10, 2);
+		});
+		
+		Schema::create('tags', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('artwork_id')->unsigned()->default(0);
+			$table->foreign('artwork_id')->references('id')->on('artworks');
+			$table->string('name');
+		});
+		
+		Schema::create('mediatypes', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+		});
+		
+		Schema::create('media', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('artwork_id')->unsigned()->default(0);
+			$table->foreign('artwork_id')->references('id')->on('artworks');
+			$table->integer('type_id')->unsigned()->default(0);
+			$table->foreign('type_id')->references('id')->on('mediatypes');
+			$table->string('link');
 		});
 		
 	}
@@ -45,7 +95,13 @@ class CreateDatabase extends Migration {
 	 */
 	public function down()
 	{
+		Schema::drop('media');
+		Schema::drop('mediatypes');
+		Schema::drop('tags');
 		Schema::drop('artworks');
+		Schema::drop('licenses');
+		Schema::drop('wishlists');
+		Schema::drop('searchhistory');
 		Schema::drop('users');
 	}
 
