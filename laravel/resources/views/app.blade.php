2455ae5c108a686bfb1318691c9244b659ac0f57
Jaemen Moore <jaemen.moore@armeta.com>
2015-03-18 15:43:51 -0500

    Login works OH NO

--- laravel/resources/views/app.blade.php
+++ laravel/resources/views/app.blade.php
@@ -6,9 +6,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>ArtMart</title>
 	
-	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="{{ URL::route('index', array()) }}/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+	<script src="{{ URL::route('index', array()) }}/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<nav class="navbar navbar-default">
@@ -25,18 +25,18 @@
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
-					<li><a href="{{ URL::route('index', array()) }}">Marketplace</a></li>
+					<li><a href="{{ URL::route('index', array()) }}">Browse Art</a></li>
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::guest())
-						<li><a href="/auth/login">Login</a></li> 
-						<li><a href="/auth/register">Register</a></li>
+						<li><a href="{{ URL::route('index', array()) }}/auth/login">Login</a></li> 
+						<li><a href="{{ URL::route('index', array()) }}/auth/register">Register</a></li>
 					@else
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
-								<li><a href="/auth/logout">Logout</a></li>
+								<li><a href="{{ URL::route('index', array()) }}/auth/logout">Logout</a></li>
 							</ul>
 						</li>
 					@endif
--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -4,18 +4,23 @@
 	<div class="container">
 		<div class="page-header">
 			<h1>ArtMart Marketplace</h1>
-			<p class="lead">Stuff goes here!</p>
 		</div>
 		@foreach(array_chunk($artworks->all(), 4) as $artworkRow)
 			<div class="row">
 				@foreach ($artworkRow as $artwork)
-					<div class="col-md-3">
-						<h3>{{ $artwork->title }}</h3>
-						<a href="{{ $artwork->media->first()->link }}">
-							<img src="{{ $artwork->media->first()->thumb }}"/>
-						</a>
-						<p>by {{ $artwork->user->name }}</p>
-						<p><b>{{ $artwork->license->acro }}</b> - ${{ $artwork->price }}</p>
+					<div class="col-sm-3">
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h3>{{ $artwork->title }}</h3>
+							</div>
+							<div class="panel-body">
+								<a href="{{ $artwork->media->first()->link }}">
+									<img src="{{ $artwork->media->first()->thumb }}"/>
+								</a>
+								<p>by {{ $artwork->user->name }}</p>
+								<p><b>{{ $artwork->license->acro }}</b> - ${{ $artwork->price }}</p>
+							</div>
+						</div>
 					</div>
 				@endforeach
 			</div>
