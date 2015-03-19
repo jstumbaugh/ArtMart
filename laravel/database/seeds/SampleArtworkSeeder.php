b504a90a14630a7799776e689610f3c1ac97c6f9
Jaemen Moore <jaemen.moore@armeta.com>
2015-03-19 08:36:46 -0500

    Made thumbnails less sucky.

--- laravel/database/seeds/SampleArtworkSeeder.php
+++ laravel/database/seeds/SampleArtworkSeeder.php
@@ -176,121 +176,121 @@ class SampleArtworkSeeder extends Seeder {
 			, 'artwork_id' => 1
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/dF2buzS.png'
-			, 'thumb'      => 'http://i.imgur.com/dF2buzSs.png'
+			, 'thumb'      => 'http://i.imgur.com/ktQue46.png'
 			],
 			[ 'id'         => 2
 			, 'artwork_id' => 2
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/Dzi9mV9.png'
-			, 'thumb'      => 'http://i.imgur.com/Dzi9mV9s.png'
+			, 'thumb'      => 'http://i.imgur.com/we6hLPG.png'
 			],
 			[ 'id'         => 3
 			, 'artwork_id' => 3
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/Y9VARi3.png'
-			, 'thumb'      => 'http://i.imgur.com/Y9VARi3s.png'
+			, 'thumb'      => 'http://i.imgur.com/zh5RDNC.png'
 			],
 			[ 'id'         => 4
 			, 'artwork_id' => 4
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/czOFgOQ.png'
-			, 'thumb'      => 'http://i.imgur.com/czOFgOQs.png'
+			, 'thumb'      => 'http://i.imgur.com/qKVAbQy.png'
 			],
 			[ 'id'         => 5
 			, 'artwork_id' => 5
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/zW7LGCQ.png'
-			, 'thumb'      => 'http://i.imgur.com/zW7LGCQs.png'
+			, 'thumb'      => 'http://i.imgur.com/mey7opp.png'
 			],
 			[ 'id'         => 6
 			, 'artwork_id' => 6
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/iyS3MrD.png'
-			, 'thumb'      => 'http://i.imgur.com/iyS3MrDs.png'
+			, 'thumb'      => 'http://i.imgur.com/fLjFzzM.png'
 			],
 			[ 'id'         => 7
 			, 'artwork_id' => 7
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/w8RbGEy.png'
-			, 'thumb'      => 'http://i.imgur.com/w8RbGEys.png'
+			, 'thumb'      => 'http://i.imgur.com/X1lnUZS.png'
 			],
 			[ 'id'         => 8
 			, 'artwork_id' => 7
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/gFFDRAQ.png'
-			, 'thumb'      => 'http://i.imgur.com/gFFDRAQs.png'
+			, 'thumb'      => 'http://i.imgur.com/PJnq1JO.png'
 			],
 			[ 'id'         => 9
 			, 'artwork_id' => 8
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/zufRy3T.png'
-			, 'thumb'      => 'http://i.imgur.com/zufRy3Ts.png'
+			, 'thumb'      => 'http://i.imgur.com/VfPUNZ6.png'
 			],
 			[ 'id'         => 10
 			, 'artwork_id' => 9
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/jxflxIj.png'
-			, 'thumb'      => 'http://i.imgur.com/jxflxIjs.png'
+			, 'thumb'      => 'http://i.imgur.com/f8Q5Zlv.png'
 			],
 			[ 'id'         => 11
 			, 'artwork_id' => 10
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/xcVFLDB.png'
-			, 'thumb'      => 'http://i.imgur.com/xcVFLDBs.png'
+			, 'thumb'      => 'http://i.imgur.com/x1jw1ob.png'
 			],
 			[ 'id'         => 12
 			, 'artwork_id' => 11
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/g1XXNRf.png'
-			, 'thumb'      => 'http://i.imgur.com/g1XXNRfs.png'
+			, 'thumb'      => 'http://i.imgur.com/ztygeQI.png'
 			],
 			[ 'id'         => 13
 			, 'artwork_id' => 12
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/fAZlUe5.png'
-			, 'thumb'      => 'http://i.imgur.com/fAZlUe5s.png'
+			, 'thumb'      => 'http://i.imgur.com/y6Y5FYc.png'
 			],
 			[ 'id'         => 14
 			, 'artwork_id' => 13
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/jsbS2CI.png'
-			, 'thumb'      => 'http://i.imgur.com/jsbS2CIs.png'
+			, 'thumb'      => 'http://i.imgur.com/UOuYrOs.png'
 			],
 			[ 'id'         => 15
 			, 'artwork_id' => 14
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/0Jm9L3M.png'
-			, 'thumb'      => 'http://i.imgur.com/0Jm9L3Ms.png'
+			, 'thumb'      => 'http://i.imgur.com/D2nDZph.png'
 			],
 			[ 'id'         => 16
 			, 'artwork_id' => 15
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/eGTDfr3.png'
-			, 'thumb'      => 'http://i.imgur.com/eGTDfr3s.png'
+			, 'thumb'      => 'http://i.imgur.com/YZkkvw3.png'
 			],
 			[ 'id'         => 17
 			, 'artwork_id' => 16
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/y5gcwnp.png'
-			, 'thumb'      => 'http://i.imgur.com/y5gcwnps.png'
+			, 'thumb'      => 'http://i.imgur.com/VIVCkm9.png'
 			],
 			[ 'id'         => 18
 			, 'artwork_id' => 17
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/VrqQood.png'
-			, 'thumb'      => 'http://i.imgur.com/VrqQoods.png'
+			, 'thumb'      => 'http://i.imgur.com/6CBwdAn.png'
 			],
 			[ 'id'         => 19
 			, 'artwork_id' => 18
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/PBCEZ3l.png'
-			, 'thumb'      => 'http://i.imgur.com/PBCEZ3ls.png'
+			, 'thumb'      => 'http://i.imgur.com/KKRD4Qm.png'
 			],
 			[ 'id'         => 20
 			, 'artwork_id' => 19
 			, 'type_id'    => 1
 			, 'link'       => 'http://i.imgur.com/awqnqBU.png'
-			, 'thumb'      => 'http://i.imgur.com/awqnqBUs.png'
+			, 'thumb'      => 'http://i.imgur.com/3ZKkntM.png'
 			],
 		);
 		DB::table('media')->insert($media);
