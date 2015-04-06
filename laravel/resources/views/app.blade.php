51e1b6d0e5012ccd5707865b9fe8592abcb68fd3
Jaemen Moore <jaemen.moore@armeta.com>
2015-04-06 17:19:50 -0500

    CSS revamp started.

--- laravel/resources/views/app.blade.php
+++ laravel/resources/views/app.blade.php
@@ -6,7 +6,10 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>ArtMart</title>
 	
-	<link rel="stylesheet" href="{{ URL::route('index', array()) }}/css/bootstrap.min.css">
+	<link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Montserrat'>
+	<link rel="stylesheet" type='text/css' href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type='text/css' href="{{ URL::route('index', array()) }}/css/bootstrap.min.css">
+	<link rel="stylesheet" type='text/css' href="{{ URL::route('index', array()) }}/css/app.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="{{ URL::route('index', array()) }}/js/bootstrap.min.js"></script>
 </head>
@@ -20,15 +23,21 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">ArtMart</a>
+				<a class="navbar-brand" href="{{ URL::route('index', array()) }}">ARTMART</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
-					<li><a href="{{ URL::route('index', array()) }}">Browse Art</a></li>
+					<li><a href="{{ URL::route('index', array()) }}">Browse</a></li>
 				</ul>
 
 				<ul class="nav navbar-nav navbar-right">
+					<li>
+						<form name = "search-form">
+							<i class="fa fa-search"></i>
+							<input type="text" name="search"/>
+						</form>
+					</li>
 					@if (Auth::guest())
 						<li><a href="{{ URL::route('index', array()) }}/auth/login">Login</a></li> 
 						<li><a href="{{ URL::route('index', array()) }}/auth/register">Register</a></li>
@@ -36,6 +45,7 @@
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
+								<li><a href="{{ URL::route('index', array()) }}/upload">Upload</a></li>
 								<li><a href="{{ URL::route('index', array()) }}/auth/logout">Logout</a></li>
 							</ul>
 						</li>
--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -3,19 +3,21 @@
 @section('content')
 	<div class="container">
 		<div class="page-header">
-			<h1>ArtMart Marketplace</h1>
+			<h1>THE MARKETPLACE</h1>
 		</div>
 		@foreach(array_chunk($artworks->all(), 4) as $artworkRow)
 			<div class="row">
 				@foreach ($artworkRow as $artwork)
-					<div class="col-md-3">
+					<div class="col-md-3 panel-linkbox">
 						<a href="{{ URL::route('index', array()) }}/artwork/{{ $artwork->id }}">
 							<div class="panel panel-default">
 								<div class="panel-heading">
 									<h3>{{ $artwork->title }}</h3>
 								</div>
 								<div class="panel-body">
-									<img src="{{ $artwork->media->first()->thumb }}"/>
+									<div class="panel-image">
+										<img src="{{ $artwork->media->first()->thumb }}"/>
+									</div>
 									<p>by {{ $artwork->user->name }}</p>
 									<p><b>{{ $artwork->license->acro }}</b> - ${{ $artwork->price }}</p>
 								</div>
