2422f8afe6765995122df5944a585fbe35254d56
Jaemen Moore <jaemen.moore@armeta.com>
2015-04-21 12:22:04 -0500

    Made upload page a bit better. Still missing some functionality.

--- laravel/app/Http/Controllers/UploadController.php
+++ laravel/app/Http/Controllers/UploadController.php
@@ -4,23 +4,62 @@ use Validator;
 use Redirect;
 use Request;
 use Session;
-class UploadController extends Controller {
-public function upload() {
-  // getting all of the post data
-  $file = array('image' => Input::file('filefield'));
-  // setting up rules
-  $name = "youMessedUpSomeWhere";
 
-$array = Input::all();
-$blah = array_slice($array, 0, 2, true);
-$name = implode("_",$blah);
+class UploadController extends Controller {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Upload Controller
+	|--------------------------------------------------------------------------
+	|
+	| This controller handles the upload form and the POST action for uploads.
+	| User is required to be authenticated to access these methods.
+	|
+	*/
+
+	/**
+	 * Create a new controller instance. Require user to be authenticated.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
+	/**
+	 * Show the upload page to the user.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return view('upload');
+	}
+
+	/**
+	 * Post the upload data to the database.
+	 *
+	 * @return Response
+	 */
+	public function upload() {
+		// get all of the post data
+		$file = array('image' => Input::file('filefield'));
+		
+		// set up rules
+		$name = "youMessedUpSomeWhere";
+		$array = Input::all();
+		$blah = array_slice($array, 0, 2, true);
+		$name = implode("_",$blah);
  
-      $destinationPath = 'uploads'; // upload path
-      $extension = Input::file('filefield')->getClientOriginalExtension(); // getting image extension
-      $fileName = $name.'.'.$extension; // renameing image
-      Input::file('filefield')->move($destinationPath, $fileName); // uploading file to given path
-      // sending back with message
-      Session::flash('success', 'Upload successfully'); 
-      return Redirect::to('home');
-    }
-  }
+		// do the upload
+		$destinationPath = 'uploads';
+		$extension = Input::file('filefield')->getClientOriginalExtension();
+		$fileName = $name.'.'.$extension;
+		Input::file('filefield')->move($destinationPath, $fileName);
+		
+		// return success
+		Session::flash('success', 'Upload successfully'); 
+		return Redirect::to('home');
+	}
+}
--- laravel/resources/views/upload.blade.php
+++ laravel/resources/views/upload.blade.php
@@ -1,14 +1,61 @@
 @extends('app')
-@section('content')
- 
-    <form action="ImageUpload" method="post" enctype="multipart/form-data">
-      <p>Artwork to Upload:  <input type="file" name="filefield"> </p>
-       <p> Name of Artwork: <input type="text" name="Artwork Name"></p>
-       <p>  Name of Artist: <input type="text" name="Artist"></p>
-       <p>Price:  <input type="text" name="Price"></p>
-       <p>Tags: <input type="text" name="tags"></p>
-        <input type="submit">
-    </form>
 
+@section('content')
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">Upload Art</div>
+				<div class="panel-body">
+					<form class="form-horizontal" role="form" method="POST" action="ImageUpload" enctype="multipart/form-data">
+						<input type="hidden" name="_token" value="{{ csrf_token() }}" />
+						
+						<div class="form-group">
+							<label class="col-md-4 control-label">Artwork to Upload:</label>
+							<div class="col-md-6">
+								<input type="file" class="form-control name="filefield" />
+							</div>
+						</div>
+						
+						<div class="form-group">
+							<label class="col-md-4 control-label">Name of Artwork:</label>
+							<div class="col-md-6">
+								<input type="text" class="form-control name="Artwork Name" />
+							</div>
+						</div>
+						
+						<div class="form-group">
+							<label class="col-md-4 control-label">Name of Artist:</label>
+							<div class="col-md-6">
+								<input type="text" class="form-control name="Artist" />
+							</div>
+						</div>
+						
+						<div class="form-group">
+							<label class="col-md-4 control-label">Price:</label>
+							<div class="col-md-6">
+								<input type="text" class="form-control name="Price" />
+							</div>
+						</div>
+						
+						<div class="form-group">
+							<label class="col-md-4 control-label">Tags:</label>
+							<div class="col-md-6">
+								<input type="text" class="form-control name="tags" />
+							</div>
+						</div>
+						
+						<div class="form-group">
+							<div class="col-md-6 col-md-offset-4">
+								<button type="submit" class="btn btn-primary" style="margin-right: 15px;">
+									Upload
+								</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 @endsection
-@stopx
